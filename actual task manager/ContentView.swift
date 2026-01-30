f20e4af3ef85c95f07274238e4156594d9eb3d4f
racerray26 <ng_yee_meng_rayson@s2025.ssts.edu.sg>
2026-01-30 11:08:55 +0800

    Initial Commit

---
+++ actual task manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  actual task manager
+//
+//  Created by Rayson Ng on 30/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ actual task manager/actual_task_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  actual_task_managerApp.swift
+//  actual task manager
+//
+//  Created by Rayson Ng on 30/1/26.
+//
+
+import SwiftUI
+
+@main
+struct actual_task_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
