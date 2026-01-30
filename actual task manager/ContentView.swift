e527e1ba367ab7252fd26ea1060e84112a292d15
racerray26 <ng_yee_meng_rayson@s2025.ssts.edu.sg>
2026-01-30 11:12:23 +0800

    acutal task manager children

--- actual task manager/ContentView.swift
+++ actual task manager/ContentView.swift
@@ -1,19 +1,47 @@
 //
 //  ContentView.swift
-//  actual task manager
+//  Task 1
 //
-//  Created by Rayson Ng on 30/1/26.
+//  Created by Rayson Ng on 12/1/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks: [String] = ["Play HSR", "Pack desk"]
+    @State var importantTasks: [String] = ["homework", "Buy groceries", "do ict"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {                   
+                Section("Important Tasks") {
+                    ForEach(importantTasks, id: \.self) { item in
+                        NavigationLink(destination: Text(item).padding()) {
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text(item)
+                                    .foregroundStyle(.red)
+                                Text("VERY IMPORTANT!!")
+                                    .font(.subheadline)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+                Section("Tasks") {
+                    ForEach(tasks, id: \.self) { item in
+                        NavigationLink(destination: Text(item).padding()) {
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text(item)
+                                    .foregroundStyle(.blue)
+                                Text("Tasks")
+                                    .font(.subheadline)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Tasks")
         }
         .padding()
     }
